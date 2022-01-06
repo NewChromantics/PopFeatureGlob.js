d08a89c102e0207ea49b6f6dc39a6662435a81a9
Graham Reeves <graham@grahamreeves.com>
2022-01-06 15:51:07 +0000

    Lots of tweaking with params, and newer appraoch to detecting output

--- GlobFeatures.frag.js
+++ GlobFeatures.frag.js
@@ -7,14 +7,14 @@ varying vec2 FragUv;
 uniform sampler2D InputTexture;
 uniform vec2 InputWidthHeight;
 //uniform sampler2D BackgroundImage;
-const float MaxLumaDiff = 0.20;
+const float MaxLumaDiff = 0.14;
 
 #define MAX_ANGLE_COUNT 90
-#define RadiusScale 1.0
-const float GlobRadiusOuterPx = 20.0 * RadiusScale;
-const float GlobRadiusMidPx = 10.0 * RadiusScale;
-const float GlobRadiusInnerPx = 5.0 * RadiusScale;
-const int MaxSequence = MAX_ANGLE_COUNT/10;			//	thicker lines and more angles need to allow more in a sequence
+#define RadiusScale 0.5
+const float GlobRadiusOuterPx = 14.0 * RadiusScale;
+const float GlobRadiusMidPx = 8.0 * RadiusScale;
+const float GlobRadiusInnerPx = 4.0 * RadiusScale;
+const int MaxIndependentSequence = MAX_ANGLE_COUNT/10;			//	thicker lines and more angles need to allow more in a sequence
 const int LineIndependentsCount = 2;	//	one in, one out
 const int TeeIndependentsCount = 3;
 const int CrossIndependentsCount = 4;	
@@ -32,27 +32,37 @@ const float RotationDeg = 0.0;
 #define RAY_MATCH			4
 #define RAY_MISMATCH		5
 
+#define GLOB_SOLID			12
 #define GLOB_TOO_NOISY		6
 #define GLOB_LONER			7
 #define GLOB_CORNER			8
 #define GLOB_LINE			9
 #define GLOB_TEE			10	//	T junction
 #define GLOB_CROSS			10	//	+ junction
+#define GLOB_EDGE			11	//	pixel between 2 sides
 
 vec3 GetGlobColour(float GlobTypef)
 {
-	int GlobShowOnly = 0;
+	int GlobShowOnly = GLOB_LINE;
 	int GlobType = int(GlobTypef);
-	if ( GlobShowOnly != 0 && GlobType != GlobShowOnly )
-		GlobType = GLOB_TOO_NOISY;
+	if ( GlobShowOnly != 0 )
+	{
+		if ( GlobType == GlobShowOnly )
+			return vec3(0,1,0);
+		return vec3(0,0,0);
+	}
 	
-	if ( GlobType == GLOB_TOO_NOISY )	return vec3(0,0,0);
+	
+	if ( GlobType == GLOB_TOO_NOISY )	return vec3(1,0,1);
+	if ( GlobType == GLOB_SOLID )		return vec3(0,0,0);
 	if ( GlobType == GLOB_LONER )		return vec3(1,0,0);
 	
 	if ( GlobType == GLOB_CORNER )		return vec3(0,1,1);
 	if ( GlobType == GLOB_LINE )		return vec3(1,1,1);
 	if ( GlobType == GLOB_TEE )			return vec3(1,1,0);
 	if ( GlobType == GLOB_CROSS )		return vec3(1,1,0);
+	
+	if ( GlobType == GLOB_EDGE )		return vec3(0,0,1);
 	
 	return vec3(1,0,1);
 }	
@@ -123,9 +133,65 @@ vec4 CalculateGlobFeature(vec2 uv,sampler2D ColourSource,out vec3 FeatureRays[MA
 		RingMatches[a+ANGLE_COUNT] = RingMatches[a];
 	}
 	
+	
+	
+	//	new version, squash down sets of matches
+	//	once we go over a certain amount, its just noise (or a star)
+	#define MAX_SEQ		(NOISE_SEQ+1)
+	#define NOISE_SEQ	6
+	//int SeqSizes[MAX_SEQ];
+	int SeqIndex = 0;
+
+	bool SeqMatch = RingMatches[0];
+	//SeqSizes[SeqIndex] = 0;
+	
+	for ( int i=0;	i<ANGLE_COUNT;	i++)
+	{
+		if ( RingMatches[i] == SeqMatch )
+		{
+			//SeqSizes[SeqIndex]++;
+		}
+		else
+		{
+			//	broken sequence
+			SeqIndex++;
+			if ( SeqIndex >= MAX_SEQ )
+				break;
+			//SeqSizes[SeqIndex] = 1;
+			SeqMatch = RingMatches[i];
+		}
+	}
+	//	loop around
+	if ( SeqIndex > 1 )
+	{
+		if ( SeqMatch == RingMatches[0] )
+		{
+			//SeqSizes[0] += SeqSizes[SeqIndex];
+			SeqIndex--;
+		}
+	}
+	
+	int SeqCount = SeqIndex+1;
+	
+	if ( SeqCount >= NOISE_SEQ )
+		return vec4( uv, float(GLOB_TOO_NOISY), 0.0 );
+		
+	if ( SeqCount == 1 && RingMatches[0] )
+		return vec4( uv, float(GLOB_SOLID), 0.0 );
+	if ( SeqCount == 1 && !RingMatches[0] )
+		return vec4( uv, float(GLOB_LONER), 0.0 );
+	//	two sides
+	if ( SeqCount == 2 )
+		return vec4( uv, float(GLOB_EDGE), 0.0 );
+	
+	//	could be line, etc
+	return vec4( uv, float(GLOB_LINE), 0.0 );
+	
+	
 	//	now calc score
-	//	count how many independent matches we get
-	//	independent match is 1 or 2 matches in sequence
+	//	count how many independent matches (sets of matches) we get
+	//	independent match is N matches in sequence
+	//	where N(MaxSequence) increases, we allow thicker lines
 	int Independents = 0;
 	int Blocks = 0;
 	int MatchCounter = 0;
@@ -142,7 +208,7 @@ vec4 CalculateGlobFeature(vec2 uv,sampler2D ColourSource,out vec3 FeatureRays[MA
 		}
 		else
 		{
-			if ( MatchCounter > MaxSequence )
+			if ( MatchCounter > MaxIndependentSequence )
 			{
 				Blocks++;
 			}
@@ -159,7 +225,7 @@ vec4 CalculateGlobFeature(vec2 uv,sampler2D ColourSource,out vec3 FeatureRays[MA
 		}
 	}
 	//	finish
-	if ( MatchCounter > MaxSequence )
+	if ( MatchCounter > MaxIndependentSequence )
 		Blocks++;
 	else if ( MatchCounter > 0 )
 		Independents++;
